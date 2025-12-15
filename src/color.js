fdc812ac3b3f93b06b5f89d4cf01606e66bc9b29
board <boardergamezdiscord@gmail.com>
2025-12-15 20:05:57 +0100

    addded my icons and uhh created color.js for the cattpuccin colors

---
+++ src/color.js
@@ -0,0 +1,16 @@
+export const Cattpuccin = {
+    Rosewater : '#dc8a78',
+    Flamingo : '#dd7878',
+    Pink : '#ea76cb',
+    Mauve : '#8838ef',
+    Red : '#d20f39',
+    Maroon : '#e64553',
+    Peach : '#fe640b',
+    Yellow : '#df8e1d',
+    Green : '#40a02b',
+    Teal : '#179299',
+    Sky : '#04a5e5',
+    Sapphire : '#209fb5',
+    Blue : '#1e66f5',
+    Lavender : '#7287fd'
+}
