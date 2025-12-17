5713e2bff71849e88a85dde55c88f7e7fc896cd7
board <boardergamezdiscord@gmail.com>
2025-12-17 22:37:53 +0100

    CHANGED SOMETHING

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,11 +1,11 @@
 <script>
-
+    import froggyLogo from '../assets/Froggy logo.svg';
+    import { Catppuccin } from '../color.js';
 </script>
 
-<header class="site-header">
-    <div class="logo">
-        <p>Froggy :3</p>
-    </div>
+<header class="site-header" style="background-color: {Catppuccin.Mantle};">
+    <img src={froggyLogo} alt="Froggy Logo" class="logo-img" />
+    <p class="title" style="color: {Catppuccin.Green};">Froggy :3</p>
 </header>
 <style>
     .site-header{
@@ -14,11 +14,21 @@
         top: 0;
         left: 0;
         width: 100%;
-        justify-content: space-evenly;
+        height: 60px;
+        justify-content: center;
         align-items: center;
-        padding: 10px 20px;
-        background-color: #eff1f5;
-        color: #5c5f77;
+        padding: 0 20px;
         z-index: 100;
+    }
+    .logo-img {
+        position: absolute;
+        left: 10px;
+        height: 100%;
+        padding: 5px;
+    }
+    .title {
+        margin: 0;
+        font-weight: bold;
+        font-size: 1.5rem;
     }
 </style>
